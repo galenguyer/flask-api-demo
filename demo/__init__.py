d1c7140506fb4640a55169b0ecb1472a3ca1de55
Galen Guyer <galen@galenguyer.com>
2020-10-25 12:23:20 -0400

    remove HTML bits

--- demo/__init__.py
+++ demo/__init__.py
@@ -3,7 +3,7 @@
 import os
 import subprocess
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, jsonify
 
 APP = Flask(__name__)
 
@@ -15,18 +15,6 @@ else:
 
 APP.secret_key = APP.config['SECRET_KEY']
 
-@APP.route('/static/<path:path>', methods=['GET'])
-def _send_static(path):
-    return send_from_directory('static', path)
-
 @APP.route('/')
 def _index():
-    commit_hash = None
-    try:
-        commit_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']) \
-                                .strip() \
-                                .decode('utf-8')
-    # pylint: disable=bare-except
-    except:
-        commit_hash = None
-    return render_template('home.html', commit_hash=commit_hash)
+    return jsonify(status=200, response="OK")
